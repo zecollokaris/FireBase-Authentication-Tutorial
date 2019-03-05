fcce6a907c5f78c8cabbfb87bb0ca7b2d063cc82
zecollokaris <collo.kariss@gmail.com>
2019-03-05 16:34:05 +0300

    added login activity class and layout

---
+++ app/src/main/java/com/zecolloauth/zecolloauth/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.zecolloauth.zecolloauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
