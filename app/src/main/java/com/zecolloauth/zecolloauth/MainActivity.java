eef888e2bc374759340f78d7b6b868e7913c0423
zecollokaris <collo.kariss@gmail.com>
2019-03-07 11:56:12 +0300

    changed theme for pop up

--- app/src/main/java/com/zecolloauth/zecolloauth/MainActivity.java
+++ app/src/main/java/com/zecolloauth/zecolloauth/MainActivity.java
@@ -3,6 +3,9 @@ package com.zecolloauth.zecolloauth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
