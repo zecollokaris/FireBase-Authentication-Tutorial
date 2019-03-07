6fc08523d4bcd1af4a09d46afa728cd035908099
zecollokaris <collo.kariss@gmail.com>
2019-03-07 11:30:41 +0300

    added menu layout

--- app/src/main/java/com/zecolloauth/zecolloauth/MainActivity.java
+++ app/src/main/java/com/zecolloauth/zecolloauth/MainActivity.java
@@ -2,12 +2,17 @@ package com.zecolloauth.zecolloauth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 }
